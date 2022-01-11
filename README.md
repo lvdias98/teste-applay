b8250f1177c71befb9a26c5d17d99572a2e1fad3
lvdias98 <lvdias98@gmail.com>
2022-01-11 15:08:40 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Teste Applay
+
+Chegou uma boa oportunidade para explorar seu conhecimento em Front-End. Nesta etapa, precisamos que você analise o módulo existente e ajuste algumas funcionalidades da tela utilizando seus conhecimentos.
+
+## Tarefas
+  - Melhorar o layout com bootstrap (Ex: https://getbootstrap.com/)
+  - Adicionar máscara nos campos CPF e Celular (Ex: https://igorescobar.github.io/jQuery-Mask-Plugin/docs.html)
+  - Adicionar o checkbox para bootstap nas habilidades
+  - Validar formulário com JS
+  - Persistência de dados (Ex: Armazenamento no local storage)
+  - Ajustar a funcionalidade de remover
+  - Subir o resultado final no seu GitHub (deixar público)
+
+## Diferenciais
+  - Utilizar ícones nos botões (Ex: https://fontawesome.com/)
+  - Utilizar alertas customizados (Ex: https://sweetalert2.github.io/)
+  - Utilizar estilos próprios
+
+## Layout de exemplo com bootstrap
+![alt text](https://imgur.com/dVmkO3s)
+![alt text](https://imgur.com/EC2t5un)
+
+
---
+++ assets/script.js
@@ -0,0 +1,172 @@
+let candidatos = [
+  { id: "1", cpf: "42604610876", nome: "Lucas Vieira Dias", celular: "11957770782", email: "lvdias98@gmail.com", sexo: "Masculino", nascimento: "01/12/1998", skills: { html: true, css: true, js: true } },
+  { id: "2", cpf: "42604610876", nome: "Nelson Santana", celular: "11957770782", email: "lvdias98@gmail.com", sexo: "Masculino", nascimento: "01/12/1998", skills: { html: true, css: true, js: true } },
+];
+
+function abrirModal(candidato) {
+  if (candidato) {
+    document.getElementById("id").value = candidato.id;
+    document.getElementById("cpf").value = candidato.cpf;
+    document.getElementById("nome").value = candidato.nome;
+    document.getElementById("celular").value = candidato.celular;
+    document.getElementById("email").value = candidato.email;
+    if(candidato.sexo=='Masculino'){
+      document.getElementById("sexoMasculino").checked = true;
+    }else{
+      document.getElementById("sexoFeminino").checked = true;
+    }
+    document.getElementById("nascimento").value = candidato.nascimento.split('/').reverse().join('-');
+    document.getElementById("skillHtml").checked = candidato.skills.html;
+    document.getElementById("skillCss").checked = candidato.skills.css;
+    document.getElementById("skillJs").checked = candidato.skills.js;
+  }
+
+  $('#candidatoModal').modal('show');
+}
+
+function fecharModal() {
+  $('#candidatoModal').modal('hide');
+  $('body').removeClass('modal-open');
+  $('body').removeAttr('style');
+  $('.modal-backdrop').remove();
+
+  document.getElementById("id").value = "";
+  document.getElementById("cpf").value = "";
+  document.getElementById("nome").value = "";
+  document.getElementById("celular").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("sexoMasculino").checked = true;
+  document.getElementById("nascimento").value = '';
+  document.getElementById("skillHtml").checked = false;
+  document.getElementById("skillCss").checked = false;
+  document.getElementById("skillJs").checked = false;
+}
+
+function salvar() {
+  let id = document.getElementById("id").value;
+  let cpf = document.getElementById("cpf").value;
+  let nome = document.getElementById("nome").value;
+  let celular = document.getElementById("celular").value;
+  let email = document.getElementById("email").value;
+  let nascimento = document.getElementById("nascimento").value.split('-').reverse().join('/');
+  let sexo = document.getElementById("sexoMasculino").checked;
+  let skillHtml = document.getElementById("skillHtml").checked;
+  let skillCss = document.getElementById("skillCss").checked;
+  let skillJs = document.getElementById("skillJs").checked;
+
+  // Fazer validações aqui
+
+  // Fazer validações aqui
+
+  let candidato = {
+    id: id!=''?id:new Date().getTime(),
+    cpf: cpf,
+    nome: nome,
+    celular: celular,
+    email: email,
+    sexo: sexo?'Masculino':'Feminino',
+    nascimento: nascimento,
+    skills: {
+      html: skillHtml,
+      css: skillCss,
+      js: skillJs
+    }
+  };
+
+  if(id!=''){
+    let checkCandidato = candidatos.find(e=>e.id == candidato.id);
+    checkCandidato.cpf = candidato.cpf;
+    checkCandidato.nome = candidato.nome;
+    checkCandidato.celular = candidato.celular;
+    checkCandidato.email = candidato.email;
+    checkCandidato.sexo = candidato.sexo;
+    checkCandidato.nascimento = candidato.nascimento;
+    checkCandidato.skills = candidato.skills;
+  }else{
+    candidatos.push(candidato);
+  }
+
+
+  fecharModal();
+  listarCandidatos();
+}
+
+function listarCandidatos() {
+  let tabela = document.getElementById("table-body");
+  tabela.innerHTML = '';
+
+  for (let candidato of candidatos) {
+    let linha = document.createElement("tr");
+
+    let colunaCpf = document.createElement("td");
+    let colunaNome = document.createElement("td");
+    let colunaCelular = document.createElement("td");
+    let colunaEmail = document.createElement("td");
+    let colunaSexo = document.createElement("td");
+    let colunaNascimento = document.createElement("td");
+    let colunaSkills = document.createElement("td");
+    let colunaEditar = document.createElement("td");
+    let colunaRemover = document.createElement("td");
+
+    // Funcionalidades botão editar
+    let botaoEditar = document.createElement("button");
+    botaoEditar.innerHTML = 'Editar';
+    botaoEditar.onclick = function () {
+      console.log('editar');
+      abrirModal(candidato);
+    }
+
+    // Funcionalidades botão remover
+    let botaoRemover = document.createElement("button");
+    botaoRemover.innerHTML = 'Remover';
+    botaoRemover.onclick = function () {
+      alert('Erro ao remover!');
+    }
+
+    let arrSkills = [];
+    if(candidato.skills.html){
+      arrSkills.push('HTML');
+    }
+    if(candidato.skills.css){
+      arrSkills.push('CSS');
+    }
+    if(candidato.skills.js){
+      arrSkills.push('JS');
+    }
+
+    colunaCpf.appendChild(document.createTextNode(candidato.cpf));
+    colunaNome.appendChild(document.createTextNode(candidato.nome));
+    colunaCelular.appendChild(document.createTextNode(candidato.celular));
+    colunaEmail.appendChild(document.createTextNode(candidato.email));
+    colunaSexo.appendChild(document.createTextNode(candidato.sexo));
+    colunaNascimento.appendChild(document.createTextNode(candidato.nascimento));
+    colunaSkills.appendChild(document.createTextNode(arrSkills.join(', ')));
+    colunaEditar.appendChild(botaoEditar);
+    colunaRemover.appendChild(botaoRemover);
+
+    linha.appendChild(colunaCpf);
+    linha.appendChild(colunaNome);
+    linha.appendChild(colunaCelular);
+    linha.appendChild(colunaEmail);
+    linha.appendChild(colunaSexo);
+    linha.appendChild(colunaNascimento);
+    linha.appendChild(colunaSkills);
+    linha.appendChild(colunaEditar);
+    linha.appendChild(colunaRemover);
+
+    tabela.appendChild(linha);
+  }
+}
+
+listarCandidatos();
+
+
+//Trecho resposável pelo filtro da tabela
+$(document).ready(function () {
+  $("#search").on("keyup", function () {
+    var value = $(this).val().toLowerCase();
+    $("#candidatos tbody tr").filter(function () {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+    });
+  });
+});
